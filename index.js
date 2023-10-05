978c57483589a1d56c9a1bf13181bb662f856cc3
Marozva <isabokefrankline@gmail.com>
2023-10-05 13:15:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+// Access global variable
+function upperCaseCustomerName(){
+    if (customerName){
+        customerName = customerName.toUpperCase();
+    }
+}
+
+//  A variable in global scope
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+// Overwrite the above function
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+//
+const leastFavoriteCustomer = 'John';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Smith';
+}
+changeLeastFavoriteCustomer();
